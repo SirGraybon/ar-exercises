92f6ba4bbcf70d2aeff90d37e732b6864cb7407a
SirGraybon <graydonritchie@gmail.com>
2023-05-29 20:46:35 -0400

    completes exercise 6

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,6 @@
 class Store < ActiveRecord::Base
-  `has_many :employees`
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { only_integer: true, less_than_or_equal_to: 200, greater_than_or_equal_to: 40, }
+  has_many :employees
 end
